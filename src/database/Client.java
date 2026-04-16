b8d4b25bb4793c16661fe9c693149e45c9d8f630
gih <giovannagaeta15@gmail.com>
2026-04-16 11:37:47 -0300

    adicionei os metodos faltantes da classe cliente, agora preciso testar

--- src/database/Client.java
+++ src/database/Client.java
@@ -2,6 +2,8 @@ package src.database;
 
 import java.net.*;
 import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Client {
 
@@ -38,6 +40,52 @@ public class Client {
         try {
             out.println("g" + " " + key);
             String response = in.readLine();
+            return response;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public String remove(String key){
+        try {
+            out.println("r" + " " + key);
+            String response = in.readLine();
+            return response;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public String clear(){
+        try {
+            out.println("c");
+            String response = in.readLine();
+            return response;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public String checkpoint(){
+        try {
+            out.println("k");
+            String response = in.readLine();
+            return response;
+        } catch (IOException e) {
+            return null;
+        }
+    }
+
+    public List<String> list(){
+        try {
+            List<String> response = new ArrayList<>();
+            out.println("l");
+            String line;
+            while((line = in.readLine()) != null) {
+                if (line.equals("END_OF_LIST"))
+                    break;
+                response.add(line);
+            }
             return response;
         } catch (IOException e) {
             return null;
